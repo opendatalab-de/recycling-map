784fb487323100efa6637f57a60110feefd4da3c
Felix Ebert <felix.ebert@if-core.de>
2012-12-06 17:18:10 +0100

    details grabber

--- recycling-import/src/main/java/de/grundid/schlemmerblock/DetailGrabber.java
+++ recycling-import/src/main/java/de/grundid/schlemmerblock/DetailGrabber.java
@@ -1,14 +1,12 @@
 package de.grundid.schlemmerblock;
 
-import com.gargoylesoftware.htmlunit.html.DomElement;
-import com.gargoylesoftware.htmlunit.html.DomNodeList;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class DetailGrabber {
 
-	public SbAmenity getSbAmenity(HtmlPage htmlPage) {
-
-		DomNodeList<DomElement> elementsByTagName = htmlPage.getElementsByTagName("div");
+	public SbAmenity getSbAmenity(Document document) {
+		Elements elementsByTagName = document.getElementsByTag("title");
 		System.out.println(elementsByTagName.size());
 		return null;
 	}
