1fb07a7f8dea7310bff19c8ac86fe92b0090541d
Adrian Stabiszewski <as@grundid.de>
2012-12-06 15:20:21 +0100

    repo update

---
+++ recycling-import/src/main/java/de/grundid/schlemmerblock/DetailListGrabber.java
@@ -0,0 +1,14 @@
+package de.grundid.schlemmerblock;
+
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+
+public class DetailListGrabber {
+
+	public List<String> getUrls() {
+		WebClient webClient = new WebClient();
+		//		HtmlPage page = webClient.getp
+		return null;
+	}
+}
