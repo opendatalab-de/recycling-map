4a5aff7246928e8e703fe80c02a62f9eb5a52f30
Adrian Stabiszewski <as@grundid.de>
2012-12-06 16:09:58 +0100

    some tests

--- recycling-import/src/main/java/de/grundid/schlemmerblock/DetailListGrabber.java
+++ recycling-import/src/main/java/de/grundid/schlemmerblock/DetailListGrabber.java
@@ -3,7 +3,12 @@ package de.grundid.schlemmerblock;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.w3c.dom.Node;
+
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.DomNodeList;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class DetailListGrabber {
@@ -21,6 +26,20 @@ public class DetailListGrabber {
 
 	protected List<String> parsePage(HtmlPage page) {
 		List<String> urls = new ArrayList<String>();
+		DomNodeList<DomElement> selectElement = page.getElementsByTagName("*");
+
+		for (DomElement domElement : selectElement) {
+			String attribute = domElement.getAttribute("class");
+			if (attribute.equals("oswselect")) {
+				DomNodeList<DomNode> childNodes = domElement.getChildNodes();
+				for (DomNode domNode : childNodes) {
+					Node namedItem = domNode.getAttributes().getNamedItem("value");
+					String nodeValue = namedItem.getNodeValue();
+					System.out.println(nodeValue);
+				}
+			}
+		}
+
 		return urls;
 	}
 }
--- recycling-import/src/test/java/de/grundid/schlemmerblock/DetailListGrabberTest.java
+++ recycling-import/src/test/java/de/grundid/schlemmerblock/DetailListGrabberTest.java
@@ -1,14 +1,20 @@
 package de.grundid.schlemmerblock;
 
+import static org.junit.Assert.*;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.List;
 
 import org.junit.Test;
 
 import com.gargoylesoftware.htmlunit.StringWebResponse;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HTMLParser;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class DetailListGrabberTest {
@@ -18,13 +24,22 @@ public class DetailListGrabberTest {
 		DetailListGrabber grabber = new DetailListGrabber();
 
 		WebClient webClient = new WebClient();
+		webClient.setJavaScriptEnabled(false);
 
 		String data = loadFile("Regionsuche.htm");
-		URL url = new URL("http://localhost");
+		URL url = new URL("http://www.schlemmerblock.de/Regionsuche");
 		StringWebResponse webResponse = new StringWebResponse(data, url);
-		HtmlPage htmlPage = new HtmlPage(url, webResponse, webClient.getCurrentWindow());
+		//		HtmlPage htmlPage = webClient.getPage(url);
 
-		grabber.parsePage(htmlPage);
+		HtmlPage htmlPage = HTMLParser.parseHtml(webResponse, webClient.getCurrentWindow());
+		//		HtmlPage htmlPage = new HtmlPage(url, webResponse, webClient.getCurrentWindow());
+
+		HtmlElement body = htmlPage.getBody();
+		System.out.println(body);
+
+		List<String> list = grabber.parsePage(htmlPage);
+
+		assertEquals(20, list.size());
 
 	}
 
@@ -32,7 +47,7 @@ public class DetailListGrabberTest {
 		try {
 			StringBuilder sb = new StringBuilder();
 			InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(filename);
-			BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+			BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF8")));
 
 			while (br.ready()) {
 				String line = br.readLine();
