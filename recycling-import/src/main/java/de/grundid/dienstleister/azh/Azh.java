38b59d51e1dd31e13b54d9c213e864fc174e12f2
Adrian Stabiszewski <as@grundid.de>
2014-06-26 16:15:06 +0200

    azh

---
+++ recycling-import/src/main/java/de/grundid/dienstleister/azh/Azh.java
@@ -0,0 +1,72 @@
+package de.grundid.dienstleister.azh;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.grundid.dienstleister.ServiceProvider;
+import de.grundid.dienstleister.UrlWithCategory;
+import de.grundid.recycling.ResourceUtils;
+import de.grundid.utils.GrabUtils;
+
+public class Azh {
+
+	private void run() {
+		List<ServiceProvider> spAll = new ArrayList<ServiceProvider>();
+		Set<UrlWithCategory> uniqueUrls = getUniqueUrls();
+		int counter = 0;
+		long startTime = System.currentTimeMillis();
+		for (UrlWithCategory urlWithCategory : uniqueUrls) {
+			counter++;
+			long time = System.currentTimeMillis() - startTime;
+			System.out.println(counter + " / " + (time / 60000) + "min => " + urlWithCategory.getUrl());
+			try {
+				Document page = LinkScraper.parse(urlWithCategory.getUrl());
+				ServiceProvider sp = new ServiceProvider();
+				sp.setCategory(urlWithCategory.getCategory());
+				sp.setSource(urlWithCategory.getUrl());
+				Elements tables = page.getElementsByClass("table_v1");
+				Element firstTable = tables.first();
+				DetailScraper.populateData(sp, firstTable);
+				DetailScraper.populatePhone(sp, firstTable.nextElementSibling());
+				spAll.add(sp);
+			}
+			catch (IOException e) {
+				System.out.println(urlWithCategory.getUrl() + " => " + e.getMessage());
+			}
+			if (counter > 200)
+				break;
+		}
+		GrabUtils.writeJsonObject(spAll, "azh-service-provider.json");
+	}
+
+	private Set<UrlWithCategory> getUniqueUrls() {
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			UrlWithCategory[] urls = objectMapper.readValue(
+					ResourceUtils.getResourceAsString("azh-links.json", "UTF-8"), UrlWithCategory[].class);
+			System.out.println("Loaded: " + urls.length);
+			Set<UrlWithCategory> uniqueUrls = new HashSet<UrlWithCategory>();
+			for (UrlWithCategory urlWithCategory : urls) {
+				uniqueUrls.add(urlWithCategory);
+			}
+			System.out.println("Processing: " + uniqueUrls.size());
+			return uniqueUrls;
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		new Azh().run();
+	}
+}
