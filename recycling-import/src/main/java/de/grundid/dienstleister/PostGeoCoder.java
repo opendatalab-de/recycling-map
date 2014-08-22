cffb897a145b083cac2ad71b54a8d007adb6fe94
Adrian Stabiszewski <as@grundid.de>
2014-08-22 15:21:13 +0200

    more locations added

--- recycling-import/src/main/java/de/grundid/dienstleister/PostGeoCoder.java
+++ recycling-import/src/main/java/de/grundid/dienstleister/PostGeoCoder.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.geojson.Feature;
 import org.geojson.FeatureCollection;
 import org.geojson.Point;
+import org.springframework.util.StringUtils;
 
 import de.grundid.utils.GrabUtils;
 
@@ -13,7 +14,7 @@ public class PostGeoCoder {
 
 	public void run() {
 		List<ServiceProvider> serviceProviders = GrabUtils.loadFile(GrabUtils.BASE_DIR
-				+ "/all-service-provider-1408707756563.json");
+				+ "/all-service-provider-latest2.json");
 		int counter = 0;
 		for (ServiceProvider serviceProvider : serviceProviders) {
 			if (serviceProvider.getLat() != null)
@@ -31,6 +32,12 @@ public class PostGeoCoder {
 				Point point = new Point(serviceProvider.getLng(), serviceProvider.getLat());
 				feature.setGeometry(point);
 				feature.setProperty("name", serviceProvider.getName());
+				feature.setProperty("street", serviceProvider.getStreet());
+				feature.setProperty("zip", serviceProvider.getZip());
+				feature.setProperty("city", serviceProvider.getCity());
+				feature.setProperty("phone", serviceProvider.getPhone());
+				feature.setProperty("categories",
+						StringUtils.collectionToCommaDelimitedString(serviceProvider.getCategorySet()));
 				fc.add(feature);
 			}
 		}
