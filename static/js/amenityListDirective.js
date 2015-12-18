c272252fd99256c9103a4f1925b0a68a3d5d71d4
GityUpNow <kim-cheng@gmx.de>
2015-12-18 23:39:59 +0100

    Readded the icons to google maps

--- static/js/amenityListDirective.js
+++ static/js/amenityListDirective.js
@@ -12,6 +12,7 @@
             '<div class="panel-heading">' +
             '<h3 class="panel-title">{{amenity.properties.name}}</h3>' +
             '</div><div class="panel-body">' +
+            '<a class="pull-right" href="http://maps.google.com/maps?q={{amenity.geometry.coordinates[1]}},{{amenity.geometry.coordinates[0]}}&z=15" target="gmap" title="Zu Google Maps"><img src="img/map-link.png" alt="zu Google Maps" /></a>' +
             '<ul class="compact-offer list-unstyled"><li ng-repeat="(day,segments) in amenity.properties.openingHours"><span class="uppercase">{{day | limitTo:2}}</span> von <span class="opening-time" ng-repeat="segment in segments"><span ng-if="$index !== 0"> und von </span>{{segment.from | time}} - {{segment.to | time}} Uhr</span></li>' +
             '</ul><time-ago opening-hours="amenity.properties.openingHours" name="amenity.properties.name"></time-ago></div></div>',
             link: function (scope) {
@@ -19,6 +20,8 @@
                     scope.amenities = data;
                     scope.amenities.features.forEach(function(amenity){
                         amenity.openingHours = rc.calculateOpeningHours(amenity.properties.openingHours);
+                        console.log(amenity.geometry);
+                        console.log(amenity.geometry.coordinates);
                     })
                 });
             }
